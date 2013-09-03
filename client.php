478d947a3e534d38328481b3a63717e75867d4b9
Jon Hazan <hazanjon@hotmail.com>
2013-09-03 14:04:37 +0100

    Added twitter bootstrap files

--- client.php
+++ client.php
@@ -2,32 +2,32 @@
 <head>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script src="http://js.pusher.com/2.1/pusher.min.js"></script>
+    <link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
     <script>
     
     var pusher = new Pusher('8d6d30d2c32b3a486688', { authEndpoint: '/pusher_auth.php' });
     var channel = pusher.subscribe('test_channel');
     var privateChannel = pusher.subscribe('private-channel');
-    privateChannel.bind('client-ev', function(data) {
-      alert(data);
-    });
     
     var user_id = '<?php echo $_GET['id']; ?>';
-    var left = function(){
-        privateChannel.trigger('client-ev', {id: user_id, button: 4});
-
-    }
-    var right = function(){
-        privateChannel.trigger('client-ev', {id: user_id, button: 6});
+    var control = function(num){
+        privateChannel.trigger('client-ev', {id: user_id, button: num});
 
     }
     </script>
 </head>
 <body>
 <?php echo $_GET['id']; ?>
-    <div style="width: 30%; margin: 0 auto">
-        <span style="width:30%; height:50px;" onclick="left()">Left</span>
-        <span style="width:30%; height:50px;" onclick="right()">Right</span>
-        
+    <div class="row">
+        <button class="btn btn-info span2 offset2" onclick="control(2)">Up</button>
+    </div>
+    <div class="row">
+        <button class="btn btn-info span2" onclick="control(4)">Left</button>
+        <button class="btn btn-info span2" onclick="control(5)">Fire</button>
+        <button class="btn btn-info span2" onclick="control(6)">Right</button>
+    </div>
+    <div class="row">
+        <button class="btn btn-info span2 offset2" onclick="control(8)">Down</button>
     </div>
 
 
