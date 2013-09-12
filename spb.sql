b4fd34c8fd57e23ad7d91c3e7cc893835e25c436
Curtis <ch@curtish.me>
2013-09-12 19:34:19 +0100

    added game id field to events table, new sql dump

--- spb.sql
+++ spb.sql
@@ -4,10 +4,6 @@
 -- ------------------------------------------------------
 -- Server version	5.1.69-0ubuntu0.11.10.1
 
--- User Creation too, probably be a good idea to remove this so the password aint stored
-/* CREATE USER pirate@localhost IDENTIFIED BY 'scurvydog'; */
-/* GRANT ALL ON spb.* TO pirate@localhost; */
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -18,6 +14,11 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- User Creation too, probably be a good idea to remove this so the password aint stored
+/* CREATE USER pirate@localhost IDENTIFIED BY 'scurvydog'; */
+/* GRANT ALL ON spb.* TO pirate@localhost; */
 
 --
 -- Table structure for table `events`
@@ -31,6 +32,7 @@ CREATE TABLE `events` (
   `number` varchar(30) DEFAULT NULL,
   `pickup` varchar(30) DEFAULT NULL,
   `status` int(1) DEFAULT '0',
+  `game_id` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`event_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -55,7 +57,7 @@ CREATE TABLE `games` (
   `game_id` int(11) NOT NULL AUTO_INCREMENT,
   `date` datetime DEFAULT NULL,
   PRIMARY KEY (`game_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,6 +66,7 @@ CREATE TABLE `games` (
 
 LOCK TABLES `games` WRITE;
 /*!40000 ALTER TABLE `games` DISABLE KEYS */;
+INSERT INTO `games` VALUES (1,'2013-09-09 17:55:29'),(2,'2013-09-09 17:58:15'),(3,'2013-09-09 18:02:07'),(4,'2013-09-09 18:03:20'),(5,'2013-09-09 18:03:56'),(6,'2013-09-09 18:11:07'),(7,'2013-09-12 19:02:28');
 /*!40000 ALTER TABLE `games` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,4 +106,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-09-09 17:48:33
+-- Dump completed on 2013-09-12 19:31:08
