3374cd7468db2e832523c61838acc12edb87fb60
Jon Hazan <hazanjon@hotmail.com>
2013-10-29 13:23:20 +0000

    Updated Database Template

--- spb.sql
+++ spb.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.69, for debian-linux-gnu (i686)
+-- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: spb
 -- ------------------------------------------------------
--- Server version	5.1.69-0ubuntu0.11.10.1
+-- Server version	5.5.32-0ubuntu0.12.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,11 +14,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- User Creation too, probably be a good idea to remove this so the password aint stored
-/* CREATE USER pirate@localhost IDENTIFIED BY 'scurvydog'; */
-/* GRANT ALL ON spb.* TO pirate@localhost; */
 
 --
 -- Table structure for table `events`
@@ -34,7 +29,7 @@ CREATE TABLE `events` (
   `status` int(1) DEFAULT '0',
   `game_id` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`event_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -57,7 +52,7 @@ CREATE TABLE `games` (
   `game_id` int(11) NOT NULL AUTO_INCREMENT,
   `date` datetime DEFAULT NULL,
   PRIMARY KEY (`game_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,7 +61,6 @@ CREATE TABLE `games` (
 
 LOCK TABLES `games` WRITE;
 /*!40000 ALTER TABLE `games` DISABLE KEYS */;
-INSERT INTO `games` VALUES (1,'2013-09-09 17:55:29'),(2,'2013-09-09 17:58:15'),(3,'2013-09-09 18:02:07'),(4,'2013-09-09 18:03:20'),(5,'2013-09-09 18:03:56'),(6,'2013-09-09 18:11:07'),(7,'2013-09-12 19:02:28');
 /*!40000 ALTER TABLE `games` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -85,7 +79,7 @@ CREATE TABLE `players` (
   `status` int(1) DEFAULT '0',
   `called` int(1) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -106,4 +100,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-09-12 19:31:08
+-- Dump completed on 2013-10-29 13:18:14
